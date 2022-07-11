b0b27db9f7c42c9bbec7813c6538312ccad0d8aa
Gabriela Victorio <lucerovp16@gmail.com>
2022-07-11 12:46:27 -0500

    Cambiar status de pending a delivering en vista chef

--- burger-queen/src/app/componentes/homeChef/orders-pending/orders-pending.component.ts
+++ burger-queen/src/app/componentes/homeChef/orders-pending/orders-pending.component.ts
@@ -1,5 +1,6 @@
 import { Direction } from '@angular/cdk/bidi';
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { OrdersService } from 'src/app/services/orders.service';
 
 @Component({
@@ -11,7 +12,7 @@ export class OrdersPendingComponent implements OnInit {
   direction: Direction = "ltr";
   ordersPending: any[] = [];
 
-  constructor(private ordersService:OrdersService) { }
+  constructor(private ordersService:OrdersService, private toastr: ToastrService) { }
 
   ngOnInit(): void {
     this.getAllPendingOrders()
@@ -27,6 +28,27 @@ export class OrdersPendingComponent implements OnInit {
       },
       error: (err) => {
         console.log(err, 'error mientras se hacia la consulta de data de orders');
+      }
+    })
+  }
+
+  updateOrdersStatus(id: number){
+    let date = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate(),new Date().getHours());
+    const updateData = {
+      status: 'delivering',
+      dateProcessed: date
+    }
+    this.ordersService.updateOrderMethod(updateData, id)
+    .subscribe({
+      next: () =>{
+        this.toastr.success('Orden lista', 'Lista de ordenes');
+        this.getAllPendingOrders()
+      },
+      error: (erro)=>{
+        this.toastr.error('La orden no esta lista', 'ERROR', {
+          timeOut: 3000,
+        });
+        console.log(erro);
       }
     })
   }
--- burger-queen/src/app/services/orders.service.ts
+++ burger-queen/src/app/services/orders.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class OrdersService {
-  private urlOrders = 'http://localhost:8080/orders';
+  private urlOrders = 'http://localhost:8080/orders/';
 
   constructor(private http: HttpClient) { }
 
@@ -29,5 +29,10 @@ export class OrdersService {
   // crear orden
   postOrderMethod(data: any){
   return this.http.post<any>(this.urlOrders,data, this.httpOptions())
+  }
+
+   // actualizar status
+   updateOrderMethod(orders: any, id: number) {
+    return this.http.patch<any>(this.urlOrders+id, orders, this.httpOptions())
   }
 }
