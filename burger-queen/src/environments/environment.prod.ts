fdb9a7e4f160a8705b27aa973dc0161a605eb23a
lilianaEstefanyPachari <lilianapachari21@gmail.com>
2022-07-18 09:40:48 -0500

    set de urls para ambiente dev y prod

--- burger-queen/src/environments/environment.prod.ts
+++ burger-queen/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiURL: 'https://lili-test-api-mock.herokuapp.com/',
+  apiURL: 'https://lili-test-api-mock.herokuapp.com/login',
   apiURLOrders: 'https://lili-test-api-mock.herokuapp.com/orders/',
   apiURLProducts: 'https://lili-test-api-mock.herokuapp.com/products',
   apiURLUsers: 'https://lili-test-api-mock.herokuapp.com/users/'
