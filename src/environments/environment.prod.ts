376fb3d0a6d26a620e9a239f36580d49f0556ab3
Gabriela Victorio <lucerovp16@gmail.com>
2022-07-19 18:50:15 -0500

    cambiando rutas

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiURL: 'https://lili-test-api-mock.herokuapp.com/login',
-  apiURLOrders: 'https://lili-test-api-mock.herokuapp.com/orders/',
-  apiURLProducts: 'https://lili-test-api-mock.herokuapp.com/products',
-  apiURLUsers: 'https://lili-test-api-mock.herokuapp.com/users/'
+  apiURL: 'https://gabito-test-api-mock.herokuapp.com/login',
+  apiURLOrders: 'https://gabito-test-api-mock.herokuapp.com/orders/',
+  apiURLProducts: 'https://gabito-test-api-mock.herokuapp.com//products',
+  apiURLUsers: 'https://gabito-test-api-mock.herokuapp.com/users/'
 };
