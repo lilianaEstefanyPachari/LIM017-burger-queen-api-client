a6cbdd1a478cfaca7b0c340769097464042cd5f4
Gabriela Victorio <lucerovp16@gmail.com>
2022-07-19 19:33:30 -0500

    publicando pagina en heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   apiURL: 'https://gabito-test-api-mock.herokuapp.com/login',
   apiURLOrders: 'https://gabito-test-api-mock.herokuapp.com/orders/',
-  apiURLProducts: 'https://gabito-test-api-mock.herokuapp.com//products',
+  apiURLProducts: 'https://gabito-test-api-mock.herokuapp.com/products',
   apiURLUsers: 'https://gabito-test-api-mock.herokuapp.com/users/'
 };
